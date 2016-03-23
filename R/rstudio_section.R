aeff5080062a813421237ac62533202c179de919
Colin Gillespie <csgillespie@gmail.com>
2016-03-23 17:22:17 +0000

    Add in section addin

---
+++ R/rstudio_section.R
@@ -0,0 +1,18 @@
+#' Converts text to an RStudio section
+#'
+#' This addon converts a text to an RStudio section or code folding block.
+#' @export
+rstudio_section = function() {
+
+  location = rstudioapi::getActiveDocumentContext()
+  ## Get the row number
+  r = location$selection[[1]]$range$start[1]
+  line = location$contents[r]
+  comment_line = paste0(line, " ----")
+  is_comment = substr(line, 1L, 2L) == "#'"
+
+  if(!is_comment) {
+    comment_line = paste0("# ", comment_line)
+  }
+  rstudioapi::modifyRange(c(r, 1, r, Inf), comment_line)
+}
